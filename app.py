3e85efc3b00a5216ef812ef43692a45e58665275
AJHdata <kcw467@gmail.com>
2022-04-15 16:01:49 +0900

    사진 저장 완료

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from wsgiref.handlers import read_environ
 from flask import Flask, render_template, request
 import os
+from PIL import Image
+
 
 
 app = Flask(__name__)
@@ -8,6 +11,19 @@ app = Flask(__name__)
 def index() :
     return render_template('index.html')
 
+
+@app.route('/form', methods=['GET','POST'])
+def mnist():
+    if request.method == 'GET' :
+        return render_template('form.html')
+    else :
+        # request.files[' "form.html의 input태그의 name값" ']
+        f = request.files['mnist_input']
+        # os.path.dirname(__file__) 는 현재 파일의 경로
+        path = os.path.dirname(__file__)+'/upload/' + f.filename
+        f.save(path)
+
+        return '성공!!' # 결과 확인용 return
 
 
 if __name__ == '__main__' :
